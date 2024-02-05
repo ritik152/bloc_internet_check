3cf0243f770b359502893060df1cf94325ee8e70
shaktisingh <ritik.quadtrics@gmail.com>
2024-02-05 12:19:47 +0530

    first commit

---
+++ lib/internet_bloc/check_internet_event.dart
@@ -0,0 +1,9 @@
+part of 'check_internet_bloc.dart';
+
+@immutable
+abstract class CheckInternetEvent {}
+
+
+class InternetLostEvent extends CheckInternetEvent{}
+
+class InternetGainedEvent extends CheckInternetEvent{}
